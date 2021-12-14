06c58bfa86476b7eb49d152121b66b0b2e8aa96f
k1demmin <55329467+k1demmin@users.noreply.github.com>
2021-12-14 12:49:15 -0500

    Unroll

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-mkdir -p ~/.streamlit/echo "\[server]\n\port = $PORT\n\enableCORS = false\n\headless = true\n\\n\" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+echo "[server]" >/.streamlit/config.toml
+echo "port = $PORT" >/.streamlit/config.toml
+echo "enableCORS = false" >/.streamlit/config.toml
+echo "headless = true" >/.streamlit/config.toml
